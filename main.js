0e2a71f43cd79ed5aaac152fe5fbd03f85f3ff6f
egliu <276066203@qq.com>
2017-09-29 11:44:54 +0800

    Add udp client & server.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+global.mainWindow
 
 function createWindow () {
   // Create the browser window.
@@ -65,3 +65,4 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+require('./ipc.js');
